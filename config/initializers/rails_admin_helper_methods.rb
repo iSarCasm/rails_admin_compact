648aaa03d0ac3c7ee50d9e0100967b8c9a1d1a65
SarCasm <sarcasm008@gmail.com>
2017-11-14 15:58:58 +0200

    Fix

--- config/initializers/rails_admin_helper_methods.rb
+++ config/initializers/rails_admin_helper_methods.rb
@@ -32,6 +32,7 @@ Rails.application.config.to_prepare do
     def config_template(fields, options)
       self.define_singleton_method(:for_field) do |field_name, &block|
         field = template_field(fields, field_name)
+        raise "#{field_name} not found" unless field
         css   = template_css(field)
         id    = "#{template_dom_id(field, options)}_field"
         label = label(field_name, capitalize_first_letter(field.label), class: 'control-label')
@@ -42,7 +43,7 @@ Rails.application.config.to_prepare do
     def config_show_template(fields)
       self.define_singleton_method(:for_field) do |field_name, &block|
         field = template_field(fields, field_name)
-        raise "#{field_name} not found" if field.nil?
+        raise "#{field_name} not found" unless field
         css   = "#{field.type_css_class} #{field.css_class}"
         id    = "#{field_name.to_s}_id"
         label = field.label
