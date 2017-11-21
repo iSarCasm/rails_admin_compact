daef5ab484e1fb958a244b9b6500e8308a3236eb
SarCasm <sarcasm008@gmail.com>
2017-11-21 21:44:10 +0200

    Better error message

--- config/initializers/rails_admin_helper_methods.rb
+++ config/initializers/rails_admin_helper_methods.rb
@@ -43,7 +43,7 @@ Rails.application.config.to_prepare do
     def config_show_template(fields)
       self.define_singleton_method(:for_field) do |field_name, &block|
         field = template_field(fields, field_name)
-        raise "#{field_name} not found" unless field
+        raise "Field '#{field_name}' not found, available fields are #{fields}" unless field
         css   = "#{field.type_css_class} #{field.css_class}"
         id    = "#{field_name.to_s}_id"
         label = field.label
